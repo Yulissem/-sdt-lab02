2720e74064c8a6fc3438b27fe38c4b1608ae3292
Юлия Семышева <SemyshevaYV@mpei.ru>
2018-05-22 21:53:57 +0300

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,16 @@
+ #include <iostream>
+
+ using namespace std;
+
+ int main()
+ {
+ int a, b;
+ cout << "Enter A and B: ";
+ cin >> a >> b;
+     cout << "A + B = " << a + b << '\n';
+     cout<<"2A="<<2*a;
+     cout<<"A-B"<<a-b;
+  cout<<"A*B"<<a*b;
+     cout<<"A/B"<<a/b;
+
+ }
