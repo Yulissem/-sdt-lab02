59805c7ea31ed53f158e19912bf9c832c2dae79b
Юлия Семышева <SemyshevaYV@mpei.ru>
2018-05-23 01:19:12 +0300

    main.cpp v2

--- main.cpp
+++ main.cpp
@@ -7,10 +7,5 @@
  int a, b;
  cout << "Enter A and B: ";
  cin >> a >> b;
-     cout << "A + B = " << a + b << '\n';
-     cout<<"2A="<<2*a;
-     cout<<"A-B"<<a-b;
-  cout<<"A*B"<<a*b;
-     cout<<"A/B"<<a/b;
 
  }
