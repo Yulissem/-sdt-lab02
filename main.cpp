ff6dc43d05dd1643d2f0e4e318030e6d2f4b05cf
Юлия Семышева <SemyshevaYV@mpei.ru>
2018-05-23 15:48:33 +0300

    a/b

--- main.cpp
+++ main.cpp
@@ -8,10 +8,10 @@
  {
  int a, b;
  cout << "Enter A and B: ";
- cin >> a >> b;
+     cin >> a >> b;
      cout << "A + B = " << a + b << '\n'
           << "A * 2 = " << a * 2 << '\n'
              << "A - B = " << a - b << '\n'
-          << "A * B = " << a * b << '\n';
-
+          << "A * B = " << a * b << '\n'
+          <<"A/B"<<a/b;
  }
