5225c7a46e8a89249e9d162133711d6485b7030a
u211-07 <u211-07@public.mpei.local>
2018-05-23 14:42:42 +0300

    a*b

--- main.cpp
+++ main.cpp
@@ -9,8 +9,9 @@
  int a, b;
  cout << "Enter A and B: ";
  cin >> a >> b;
-     cout << "A + B = " << a + b << '\n';
-     cout<<"2A="<<2*a;
-     cout<<"A-B"<<a-b;
+     cout << "A + B = " << a + b << '\n'
+          << "A * 2 = " << a * 2 << '\n'
+             << "A - B = " << a - b << '\n'
+          << "A * B = " << a * b << '\n';
 
  }
